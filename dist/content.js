2e3d02b3ccdd529ba72e02c7591e3da200d88719
Sven <sven@brightanalytics.eu>
2024-01-12 13:29:16 +0100

    build: 2.0.0

---
+++ dist/content.js
@@ -0,0 +1,22 @@
+if (window.location.host.includes('gitlab.')) {
+    const contentWrapperDiv = document.querySelector('.layout-page .content-wrapper');
+    const computedStyle = window.getComputedStyle(contentWrapperDiv);
+    const paddingRight = computedStyle.getPropertyValue('padding-right')
+        .replace('px', '');
+
+    const appContainer = document.createElement('div');
+    appContainer.style.right = parseInt(paddingRight || 0) + 16 + 'px';
+    appContainer.id = 'chrome-gitlab-enhancer';
+    document.body.appendChild(appContainer);
+
+    const cssScript = document.createElement('link');
+    cssScript.rel = 'stylesheet';
+    cssScript.type = 'text/css';
+    cssScript.href = chrome.runtime.getURL('/assets/main.css');
+    document.body.appendChild(cssScript);
+
+    const appScript = document.createElement('script');
+    appScript.src = chrome.runtime.getURL('/assets/main.js');
+    appScript.type = 'module';
+    document.body.appendChild(appScript);
+}
