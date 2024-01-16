7a55a4a1bb5590da2a52bb9a610616396a7e241d
puyt <svendepondt@gmail.com>
2024-01-16 20:47:30 +0100

    *change(permission): request optional permissions for content scripts
    Issue: #1

--- public/background.js
+++ public/background.js
@@ -20,5 +20,36 @@ function onWebRequestCompleted(details) {
     });
 }
 
-// chrome.tabs.onUpdated.addListener(onTabUpdated);
+function getCustomDomains(callback) {
+    chrome.storage.local.get(['customGitlabDomains'], function(result) {
+        if (result.customGitlabDomains && result.customGitlabDomains.length > 0) {
+            callback(result.customGitlabDomains.split(','));
+        }
+    });
+}
+
 chrome.webRequest.onCompleted.addListener(onWebRequestCompleted, { urls: ['<all_urls>'] });
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status !== 'complete') {
+        return;
+    }
+
+    getCustomDomains((domains) => {
+        if (!tab.url) {
+            return
+        }
+
+        try {
+            const tabUrl = new URL(tab.url);
+            const matchingDomain = domains.find((domain) => domain.includes(tabUrl.origin));
+
+            if (matchingDomain) {
+                chrome.scripting.executeScript({
+                    target: { tabId: tabId },
+                    files: ['/content.js']
+                });
+            }
+        } catch(_) { /* empty */ }
+    });
+});
