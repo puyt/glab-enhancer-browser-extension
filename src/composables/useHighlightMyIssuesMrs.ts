201158478637ff9017b65f452ea0493a5d5ab365
puyt <svendepondt@gmail.com>
2024-07-03 21:35:45 +0200

    *fix: fix highlight my issues & MRs with for GitLab 17.x
    Issue: #23

--- src/composables/useHighlightMyIssuesMrs.ts
+++ src/composables/useHighlightMyIssuesMrs.ts
@@ -20,27 +20,38 @@ export function useHighlightMyIssuesMrs() {
             if (window.location.href.includes('boards')) {
                 const cardElements = document.querySelectorAll(`.board-card a[href="/${username}"]`);
                 cardElements.forEach((element) => {
-                    const parentElement = element.closest('li.board-card');
-                    parentElement?.classList.add('gl-border', 'gl-border-2', 'gl-border-dashed', 'gl-border-purple-700');
+                    const parentElement = element.closest('li.board-card') as HTMLElement | null;
+
+                    if (parentElement?.style) {
+                        parentElement.style.border = '2px dashed #5943b6';
+                    }
                 });
             }
 
             if (window.location.href.includes('issues') && !window.location.href.includes('dashboard')) {
                 const avatarElements = document.querySelectorAll(`.issuable-meta a.gl-avatar-link[href$="/${username}"]`);
                 avatarElements.forEach((element) => {
-                    const parentElement = element.closest('.issuable-info-container')
-                        ?.closest('li');
-                    parentElement?.classList.add('gl-border!', 'gl-border-2!', 'gl-rounded-base!', 'gl-border-dashed!', 'gl-border-purple-700!', 'gl-m-3!');
+                    const parentElement = element.closest('li.issue') as HTMLElement | null;
+
+                    if (parentElement?.style) {
+                        parentElement.style.border = '2px dashed #5943b6';
+                        parentElement.style.borderRadius = '0.25rem';
+                        parentElement.style.margin = '0.5rem';
+                    }
                 });
             }
         }
 
         if (isHighlightMyMrEnabled.value && window.location.href.includes('merge_requests') && !window.location.href.includes('dashboard')) {
-            const avatarElements = document.querySelectorAll(`.issuable-meta li:not(.issuable-reviewers) a.author-link[href$="/${username}"]`);
+            const avatarElements = document.querySelectorAll(`.issuable-info-container li:not(.issuable-reviewers) a.author-link[href$="/${username}"]`);
             avatarElements.forEach((element) => {
-                const parentElement = element.closest('.issuable-info-container')
-                    ?.closest('li');
-                parentElement?.classList.add('gl-border!', 'gl-border-2!', 'gl-rounded-base!', 'gl-border-dashed!', 'gl-border-purple-700!', 'gl-m-3!');
+                const parentElement = element.closest('li.merge-request') as HTMLElement | null;
+
+                if (parentElement?.style) {
+                    parentElement.style.border = '2px dashed #5943b6';
+                    parentElement.style.borderRadius = '0.25rem';
+                    parentElement.style.margin = '0.5rem';
+                }
             });
         }
     }
